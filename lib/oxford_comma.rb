e44bc8a0d9ce6e1a52c287386b9c10c765668971
DylanToporek <dylantoporek@gmail.com>
2021-12-05 13:49:37 -0500

    commit tree f8eac48fdc29a9fd23c2836ec80e9807ea24233d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    print array
+    if array.length == 1
+        words = array.join(" ")
+        words
+    elsif array.length == 2
+        words = array.join(" and ")
+        words
+    elsif
+        array[array.length - 1] = "and " + array[array.length - 1]
+        words = array.join(", ")
+        words
+    end
 end
